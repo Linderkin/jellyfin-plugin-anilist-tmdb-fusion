5dd1317672304a905dc02ed56601f5eebe99dc6b
Linderkin <berto1989@hotmail.com>
2025-10-31 20:29:10 +0100

    Actualizo repository.json

---
+++ src/Plugin.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Common.Plugins;
+using Jellyfin.Plugin.AnilistTMDbFusion.Configuration;
+
+namespace Jellyfin.Plugin.AnilistTMDbFusion
+{
+    public class Plugin : BasePlugin, IHasWebPages, IHasPluginConfiguration
+    {
+        public static Plugin? Instance { get; private set; }
+
+        private PluginConfiguration _configuration = new PluginConfiguration();
+
+        public Plugin()
+        {
+            Instance = this;
+            SetId(new Guid("8d7a3a6d-1b23-4c77-9b8a-a12d3f4e9e7d"));
+            SetAttributes(string.Empty, string.Empty, new Version(1, 0, 0, 0));
+        }
+
+        public override string Name => "AniList + TMDb Fusion";
+        public override string Description => "Usa el título romaji de AniList y los metadatos de TMDb en español.";
+
+        public BasePluginConfiguration Configuration => _configuration;
+        public Type ConfigurationType => typeof(PluginConfiguration);
+
+        public void UpdateConfiguration(BasePluginConfiguration config)
+        {
+            _configuration = (PluginConfiguration)config;
+        }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "AnilistTMDbFusionConfig",
+                    EmbeddedResourcePath = "Jellyfin.Plugin.AnilistTMDbFusion.Configuration.PluginConfigurationPage.html"
+                }
+            };
+        }
+    }
+}
