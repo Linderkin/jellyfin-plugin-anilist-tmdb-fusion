9d4bb5f5b90d7bee9b0490336d642b3b93b4524f
Linderkin <berto1989@hotmail.com>
2025-10-31 18:23:27 +0100

    Subir versión compilada del plugin AniList + TMDb Fusion y codigo fuente

---
+++ src/Configuration/PluginConfiguration.cs
@@ -0,0 +1,10 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.AnilistTMDbFusion.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public string TmdbApiKey { get; set; } = string.Empty;
+        public bool UseRomajiTitle { get; set; } = true;
+    }
+}
